d2bb9f414d6ade71d91ea933faa221f0d3953dba
h-miiaah <haim.miiaah5@gmail.com>
2022-02-17 23:01:41 -0500

    comments

--- ExcelSheetColumnTitle.js
+++ ExcelSheetColumnTitle.js
@@ -28,6 +28,7 @@
   - For each character, we convert it to its corresponding number and add it to the result.
   - If the number is greater than 26, we divide it by 26 and keep the remainder.
   - If the number is equal to 0, we make it 26.
-  
+  - Finally return the result.
 
   */
+ 
