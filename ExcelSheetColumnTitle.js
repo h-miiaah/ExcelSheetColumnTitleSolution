076859630494a668876fe9d5ac630160fc60c94b
h-miiaah <haim.miiaah5@gmail.com>
2022-02-17 23:01:29 -0500

    comments

--- ExcelSheetColumnTitle.js
+++ ExcelSheetColumnTitle.js
@@ -27,6 +27,7 @@
   - Then iterate through the string from the end to the beginning.
   - For each character, we convert it to its corresponding number and add it to the result.
   - If the number is greater than 26, we divide it by 26 and keep the remainder.
+  - If the number is equal to 0, we make it 26.
   
 
   */
